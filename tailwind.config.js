064d7fd2419f7fd92cd6e7e6811da628622a65b1
Harsha <harshavardhanpatnana2000@gmail.com>
2023-11-18 17:54:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans-serif"],
+        serif: ["Hanken Grotesk", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
